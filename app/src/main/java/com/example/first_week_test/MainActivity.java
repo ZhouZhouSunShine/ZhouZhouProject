842b1a647bd41e53bce78e316c9030b7fde4c7c4
fanjinwei <724394700@qq.com>
2017-09-02 08:24:35 +0800

    first commit

---
+++ app/src/main/java/com/example/first_week_test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.first_week_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *
+ * 综合练习题
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //显示当前页面布局
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
